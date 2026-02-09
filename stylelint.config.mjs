bdc190970d69ccc010fc368ec3085bf7870891dc
Matthew Kwong <matthew.ws.kwong@hkjc.org.hk>
2026-02-09 15:39:43 +0800

    feat: add stylelint CSS ordering and CSS module syntax checking

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
 
 /** @type {import("stylelint").Config} */
-const stylelintConfig = { extends: ["stylelint-config-standard"] };
+const stylelintConfig = {
+  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
+  overrides: [
+    {
+      files: ["**/*.module.css"],
+      extends: ["stylelint-config-css-modules"],
+      rules: {
+        "selector-class-pattern": "^[a-z][a-zA-Z0-9]*$", // enforce CSS module class names to be in camelCase
+      },
+    },
+  ],
+};
 
 export default stylelintConfig;
